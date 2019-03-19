1337ec9c8fa231f60773f1f2b7f449a7e8c011d6
Junho So <sojunho0310@gmail.com>
2019-03-19 20:21:29 +0900

    [Add] Joseph Perth

---
+++ joseph Perth.py
@@ -0,0 +1,22 @@
+import sys
+
+num, cycle = input().split()
+queue = []
+
+num = int(num)
+cycle = int(cycle)
+
+for i in range(1, num + 1):
+    queue.append(i)
+
+sys.stdout.write("<")
+for i in range(num):
+    for j in range(cycle-1):
+        queue.append(queue[0])
+        queue.pop()
+    sys.stdout.write(str(queue[0]))
+    queue.pop()
+    if i != num:
+        sys.stdout.write(", ")
+
+sys.stdout.write(">")
