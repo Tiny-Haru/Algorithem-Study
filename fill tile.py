e2659722810a686c95fec7970b8916393b77b022
Junho So <sojunho0310@gmail.com>
2019-04-30 15:05:51 +0900

    [ADD] fill tile.py

---
+++ fill tile.py
@@ -0,0 +1,17 @@
+num = int(input())
+
+D = [0 for i in range(num + 1)]
+
+D[1] = 0
+
+if num >= 2:
+    D[2] = 3
+
+for i in range(3, num + 1):
+    D[i] = 3 * D[i - 2]
+    for j in range(i - 4, 0, -2):
+        D[i] += 2 * D[j]
+    if i % 2 == 0:
+        D[i] += 2
+
+print(str(D[num]))
